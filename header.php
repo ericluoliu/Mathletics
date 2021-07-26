1d03a21117756f4dfe608effb33def86c7ca679d
AlexNiu <38367352+AlexNiu1@users.noreply.github.com>
2021-07-25 21:38:16 -0700

    Update header.php
    Merged logo from index.html

--- header.php
+++ header.php
@@ -6,9 +6,12 @@
 <html lang="en">
     <head>
         <meta charset='utf-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Website</title>
     </head>
     <body>
+        <img class = "logo" src="Mathleticslogo.jpg" alt="Mathletics Logo">
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
