e868ce865402d5dfd2edfd9e4e25bfd5335a2576
AlexNiu <38367352+AlexNiu1@users.noreply.github.com>
2021-08-08 22:35:13 -0700

    Update index.php
    Merged index.html with index.php

--- index.php
+++ index.php
@@ -1,13 +1,44 @@
 <?php
     include_once 'header.php';
 ?>
-<?php
+
+<body>
+    <?php
     if(isset($_SESSION["useruid"])){
         echo "<p>Hello there " . $_SESSION["useruid"] . "</p";
     }
     else{
         echo "<p>Hello</p>";
     }
-?>
+    ?>
+    <br>
+    <br>
+    <div class="container-fluid">
+        <table id="catalog" class="table table-hover">  
+            <thead>  
+                <tr>  
+                    <th>Class Title</th>  
+                    <th>Subject</th>  
+                    <th>Teacher</th> 
+                    <th>other stuff </th>     
+                </tr>  
+                <tr>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                </tr>
+                <tr>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                </tr>
+            </thead>
+        </table>
+    </div>
+
+    <script src="jquery.min.cs"> </script>
+    <script src="ddtf.js"> </script>
+    <script>
+        $('#catalog').ddTableFilter():
 </body>
 </html>
