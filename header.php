19be747915abad714a51c912beda0ae825d815fe
ericluoliu <erikiceliu@gmail.com>
2021-08-12 16:16:59 -0700

    Changed CSS formatting for index.php and header.php

--- header.php
+++ header.php
@@ -1,42 +1,74 @@
-<?php 
-    session_start();
+<?php
+session_start();
 ?>
 
 <!DOCTYPE html>
+<link rel="stylesheet" href="bootstrap.min.css">
+<link rel="stylesheet" href="static/mystyle.css">
 <html lang="en">
-    <div class="container-fluid">
+<div class="header">
+    <a href='index.php'><img class="logo" src="static/Mathleticslogo.png" alt="Mathletics Logo"></a>
+    <h1>Mathletics</h1>
+    <p>Very Legit Catchphrase</p>
+</div>
+<div class="topnav">
+    <?php
+    if (isset($_SESSION["useruid"])) {
+        $param = "profile.php";
+        $param1 = "Profile";
+        $param2 = "includes/logout.inc.php";
+        $param3 = "Logout";
+    } else {
+        $param = "signup.php";
+        $param1 = "Signup";
+        $param2 = "login.php";
+        $param3 = "Login";
+    }
+    ?>
+    <a href="index.php" style="float:left">Home</a>
+    <a href="<?php echo $param; ?>" style="float:left"><?php echo $param1; ?></a>
+    <a href="<?php echo $param2; ?>" style="float:left"><?php echo $param3; ?></a>
+    <a href="index.php" style="float:left">Course Catalog</a>
+    <a href="contactform.php" style="float:left">Contact Us</a>
+
+    <!-- OLD ONE, GRACE's VERS.
+    <nav>
+        <ul id="font">
+            <li><a href="index.php">Home</a></li>
+            <?php
+            /*if (isset($_SESSION["useruid"])) {
+                echo "<li><a href='profile.php'>Profile</a></li>";
+                echo "<li><a href='includes/logout.inc.php'>Logout</a></li>";
+            } else {
+                echo "<li><a href='signup.php'>Signup</a></li>";
+                echo "<li><a href='login.php'>Login</a></li>";
+            }*/
+            ?>
+            <li class="nav-item">
+                <a class="nav-link active" href="index.php">Course Catalog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contactform.php">Contact Us</a>
+            </li>
+        </ul>
+    </nav>
+        -->
+
+</div>
+<div class="horizontal-center">
+    <div class="container">
+
         <head class="fixed-top">
             <meta charset="utf-8">
             <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-            <link rel="stylesheet" href="bootstrap.min.css">
-            <link rel="stylesheet" href="static/mystyle.css"> 
-        
-            <a href='index.php'><img class = "logo" src="static/Mathleticslogo.png" alt="Mathletics Logo"></a>
             <title> Mathletics </title>
             <br>
             <br>
-            <nav>
-            <ul id="font">
-                <li><a href="index.php">Home</a></li>
-                <?php
-                    if(isset($_SESSION["useruid"])){
-                        echo "<li><a href='profile.php'>Profile</a></li>";
-                        echo "<li><a href='includes/logout.inc.php'>Logout</a></li>";
-                    }
-                    else{
-                        echo "<li><a href='signup.php'>Signup</a></li>";
-                        echo "<li><a href='login.php'>Login</a></li>";
-                    }
-                ?>
-                <li class="nav-item">
-                    <a class="nav-link active" href="index.php">Course Catalog</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contactform.php">Contact Us</a>
-                </li>
-            </ul>
-            </nav>
+
+
         </head>
     </div>
+</div>
 
+</html>
--- index.php
+++ index.php
@@ -1,35 +1,120 @@
 <?php
-    include_once 'header.php';
+include_once 'header.php';
+?>
+<!DOCTYPE html>
+<style>
+    <?php include __DIR__ . '/static/styles.css'; ?>
+</style>
+
+<?php
+//if (isset($_SESSION["useruid"])) {
+//echo "<p>Hello there " . $_SESSION["useruid"] . "</p";
+//}
 ?>
 
-<body>
-    <?php
-    if(isset($_SESSION["useruid"])){
-        echo "<p>Hello there " . $_SESSION["useruid"] . "</p";
-    }
-    else{
-        echo "<p>Hello</p>";
-    }
-    ?>
-    <br>
-    <br>
-    <div class="container-fluid">
-        <table id="catalog" class="table table-hover">  
-            <thead>  
-                <tr>  
-                    <th>Class Title</th>  
-                    <th>Subject</th>  
-                    <th>Teacher</th> 
-                    <th>Cost </th>     
+<div class="row">
+    <div class="leftcolumn">
+        <div class="card">
+            <h2>Algebra 1</h2>
+            <h3>Taught by Professor Eric Liu</h3>
+            <h5>September 21, 2021 - December 4, 2021</h5>
+            <p>Class covers all Algebra 1 topics. Eric Liu graduated in 2016 from Bellevue College with a degree in teaching and maths.</p>
+            <div class="paybox" style="height:70px">Cost: $1000.00
+                <br>
+                <br>
+                <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHbwYJKoZIhvcNAQcEoIIHYDCCB1wCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBMZj5IvyIECvzJByLe0PsLHErQQQ+Q8mI08zTL+NvCXbSaxq1cdbqrtJWwNOWemp2JOXslzukHAv1NBpE3P9YDjbLrENoDiffHecLb/y70Sq2MeP8msEU9HRTI4nF+qf+U81O/z7veCWPHURs4Bm3f9827FpZvnG7uboosNNHTGjELMAkGBSsOAwIaBQAwgewGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIAq4PQAYHsTGAgciZuq2oZzbFH/lXvbpLNyb5dfrMn0G+SXxT8SR7AwUrUX7CwymPeA/evVrD029hnyEtob2KRZvgdFEqZdvTBhuug6muvHXDRsMMgzxv81Qe0e5vp0iDJL29LtK4SQ948cngcr6Z5YwtRJVHSn42uDApwetgSIu2ns56b8zz5hNvbEzrpw33YYzbdWC1u8Wnfl6MFVV4p8BiPD6KeKJyZispTzCshpsYeDKDJCM3S8qxPfq7ragOf7WrzL9D9b1gElLDaJ0WYwdlFaCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTIxMDgwOTIxNDYyN1owIwYJKoZIhvcNAQkEMRYEFEd5BBnuvCwu71LwWI59IEP6H49nMA0GCSqGSIb3DQEBAQUABIGAvKuENw/qYFjUOpJQ1sK3F9Et4euRPK7iXlTAOjjoFIJmFCIaNlyYsGvVRkco02PbcWs++Heh4y4+KfhmbGtN3+Ne7ijbhUp8m9NrPMRtBAIXR552cUBJwzTXeRjae9+JkMn7yPfXPdn3uU28tGWH9k6IcnEHZnLObPh8U1Jb+NA=-----END PKCS7-----">
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                </form>
+            </div>
+
+        </div>
+        <div class="card">
+            <h2>SAT Prep</h2>
+            <h3>Taught by Professor Eric Liu</h3>
+            <h5>September 21, 2021 - December 4, 2021</h5>
+            <p>Class covers all SAT topics. Eric Liu graduated in 2016 from Bellevue College with a degree in teaching and maths.</p>
+            <div class="paybox" style="height:70px">Cost: $1.00
+                <br>
+                <br>
+                <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHbwYJKoZIhvcNAQcEoIIHYDCCB1wCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCufGgbzjenbkkvNidbLQ3YMAAqRe4vRS/Kg6Q1JBkAKAx61fQintVd0RYdP0Jrp4u8kyB42RLPcTlXCho87GBEbhNVpXTAAHRsRYETFCa+xNsg0gyha/w5Z1PjHV84XNshZdHQzqgYDrGTmXjO1ksszoToaCPcdxzpq4PQGyX6TDELMAkGBSsOAwIaBQAwgewGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIWJM6KcIgbtOAgcg7fjc5mNKUjRf7PSgpkrS+LwFkKpUZ1ABbd8M+Jf4cKXdOI8bTfMVjdsf74fWeN5Gw27hU/TkN8gvbHKFb0rjcVz64H+IzSyg1oi8SHNFZvpH89GMkuQ3NtRy+5hrcF560dMvR84b4tvlwkhiL2ccXbfUHX2LdhTnuznFl0zF64DKXVlisvacPR6tjMdZOjCHFMT79vQcSe9/h9PkyxooVGUyHtn1qFb/FjYP2dytJ/RHsllcYm2J636PpoXT9OrCV7hJqgcXAG6CCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTIxMDgxMjIxNDM0M1owIwYJKoZIhvcNAQkEMRYEFEY3Wx3MpDmaTGVA6yzH21QWnrV8MA0GCSqGSIb3DQEBAQUABIGAKqAupRLeGCpYcP6Itii+9ZhlqRVfYFlegZ89iNC2Py7I9H/rLeeiVnfAYHzoilcGFORGVwDUK+BLIy7yYVv+tI5I6cO+nEbxfkTmqQw5cg8Fkw3BOFegu3W5qxFIkTePv0Jejw1w67NF+bRGMJMKEU+mFPPFdPmRJjfkPsaqTtQ=-----END PKCS7-----">
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                </form>
+            </div>
+
+        </div>
+    </div>
+    <div class="rightcolumn">
+        <div class="card">
+            <h2>About Our Courses</h2>
+            <div class="fakeimg" style="height:85px;">Our courses are professionally developed. Class content is built by highly qualified instructors to ensure a comprehensive learning experience for all students.</div>
+        </div>
+        <div class="card">
+            <h3>Our Instructors</h3>
+            <div class="row">
+                <div class="column">
+                    <div class="container" style="width:100%">
+                        <img src="test-images/akaza.jpeg" alt="Akaza" class="resized-image">
+                        <div class="overlay">
+                            <div class="text">Akaza Naoyasu</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="container" style="width:100%">
+                        <img src="test-images/shinobu.png" alt="Shinobu" class="resized-image" >
+                        <div class="overlay">
+                            <div class="text">Shinobu Kocho</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="container" style="width:100%">
+                        <img src="test-images/titan.png" alt="Titan" class="resized-image" >
+                        <div class="overlay">
+                            <div class="text">Bruh Guy</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="container" style="width:100%">
+                        <img src="test-images/eren.png" alt="Eren" class="resized-image" >
+                        <div class="overlay">
+                            <div class="text">Eren Jaeger</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+<!-- OLD ONE, GRACE'S VERS.
+<div class="horizontal-center">
+    <div class="container horizontal-center">
+        <table id="catalog" class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Class Title</th>
+                    <th>Subject</th>
+                    <th>Teacher</th>
+                    <th>Cost </th>
                     <th>Pay </th>
-                </tr>  
+                </tr>
                 <tr>
                     <td>Algebra 1</td>
                     <td>Math</td>
                     <td>Me</td>
                     <td>$1000.00</td>
                     <td>
-                        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
+                        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
                             <input type="hidden" name="cmd" value="_s-xclick">
                             <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHbwYJKoZIhvcNAQcEoIIHYDCCB1wCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBMZj5IvyIECvzJByLe0PsLHErQQQ+Q8mI08zTL+NvCXbSaxq1cdbqrtJWwNOWemp2JOXslzukHAv1NBpE3P9YDjbLrENoDiffHecLb/y70Sq2MeP8msEU9HRTI4nF+qf+U81O/z7veCWPHURs4Bm3f9827FpZvnG7uboosNNHTGjELMAkGBSsOAwIaBQAwgewGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIAq4PQAYHsTGAgciZuq2oZzbFH/lXvbpLNyb5dfrMn0G+SXxT8SR7AwUrUX7CwymPeA/evVrD029hnyEtob2KRZvgdFEqZdvTBhuug6muvHXDRsMMgzxv81Qe0e5vp0iDJL29LtK4SQ948cngcr6Z5YwtRJVHSn42uDApwetgSIu2ns56b8zz5hNvbEzrpw33YYzbdWC1u8Wnfl6MFVV4p8BiPD6KeKJyZispTzCshpsYeDKDJCM3S8qxPfq7ragOf7WrzL9D9b1gElLDaJ0WYwdlFaCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTIxMDgwOTIxNDYyN1owIwYJKoZIhvcNAQkEMRYEFEd5BBnuvCwu71LwWI59IEP6H49nMA0GCSqGSIb3DQEBAQUABIGAvKuENw/qYFjUOpJQ1sK3F9Et4euRPK7iXlTAOjjoFIJmFCIaNlyYsGvVRkco02PbcWs++Heh4y4+KfhmbGtN3+Ne7ijbhUp8m9NrPMRtBAIXR552cUBJwzTXeRjae9+JkMn7yPfXPdn3uU28tGWH9k6IcnEHZnLObPh8U1Jb+NA=-----END PKCS7-----">
                             <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" name="submit" alt="PayPal - The safer, easier way to pay online!">
@@ -48,10 +133,13 @@
             </thead>
         </table>
     </div>
-
-    <script src="jquery.min.cs"> </script>
-    <script src="ddtf.js"> </script>
-    <script>
-        $('#catalog').ddTableFilter():
+</div>
+                        -->
 </body>
+
 </html>
+
+<script src="jquery.min.cs"> </script>
+<script src="ddtf.js"> </script>
+<script>
+    $('#catalog').ddTableFilter():
