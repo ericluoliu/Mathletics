80e377533995e1d044aec6cbf5272d82330f77e3
AlexNiu <38367352+AlexNiu1@users.noreply.github.com>
2021-08-17 13:20:13 -0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 
 if(isset($_SESSION["useruid"])){
-    echo "<p>Hello there " . $_SESSION["useruid"] . "</p";
+    echo "<p>Hello there " . $_SESSION["useruid"] . " This is a " . $_SESSION["userAccountType"] . " account.</p>";
     echo "<p> This is your profile page</p>";
 }
 else{
