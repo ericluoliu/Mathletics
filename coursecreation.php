c56091ef67c4ab8fe0bd79743991533e74405e53
AlexNiu <38367352+AlexNiu1@users.noreply.github.com>
2021-08-21 13:11:52 -0700

    Update coursecreation.php

--- coursecreation.php
+++ coursecreation.php
@@ -19,26 +19,41 @@
 
     <div id="fcf-form">
     <h4 class="sent-notification"></h4>
-    <h3 class="fcf-h3">Contact Us</h3>
+    <h3 class="fcf-h3">Create a Course</h3>
+        <p>You will receive an email when your course is approved and added to the course catalog</p>
 
     <form id="fcf-form-id" method="post" action="templates/sendEmail.php" class="fcf-form-class">
         
         <div class="fcf-form-group">
-            <label for="Name" class="fcf-label">Name: </label>
+            <label for="Name" class="fcf-label">Course Name: </label>
             <div class="fcf-input-group">
                 <input type="text" id="Name" class="fcf-form-control" required>
             </div>
         </div>
 
         <div class="fcf-form-group">
-            <label for="Email" class="fcf-label">E-mail address: </label>
+            <label for="Email" class="fcf-label">Your e-mail address: </label>
+            <div class="fcf-input-group">
+                <input type="email" id="Email" class="fcf-form-control" required>
+            </div>
+        </div>
+        
+        <div class="fcf-form-group">
+            <label for="Email" class="fcf-label">Course Price (Put 0 if it is free): </label>
+            <div class="fcf-input-group">
+                <input type="email" id="Email" class="fcf-form-control" required>
+            </div>
+        </div>
+        
+        <div class="fcf-form-group">
+            <label for="Email" class="fcf-label">Type of Course (Math, Science, etc): </label>
             <div class="fcf-input-group">
                 <input type="email" id="Email" class="fcf-form-control" required>
             </div>
         </div>
 
         <div class="fcf-form-group">
-            <label for="Message" class="fcf-label">Message:</label>
+            <label for="Message" class="fcf-label">Course Description:</label>
             <div class="fcf-input-group">
                 <textarea id="Message" class="fcf-form-control" rows="6" required></textarea>
             </div>
